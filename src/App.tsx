330ebcb79dd1fb64f6f5c490945dc2deb89156c7
Dev Leslie <devonna.dev@gmail.com>
2023-04-11 13:37:34 -0400

    Add useState to populate card upon search

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,51 @@ import {
 } from "@chakra-ui/react"
 import { PokeInput } from "./components/PokeInput"
 import { PokeCard } from "./components/PokeCard"
+import { useState } from "react";
 
-// lets mockup a pokemon object
-const pokeObject = {
-  name: "Ditto",
-  type: "normal",
-  height: "3 feet",
-  weight: "40lbs",
+export const App = () => {
+  const [pokemon, setPokemon] = useState('');
+  const [pokeObject, setPokeObject] = useState({
+    name: '',
+    height: '',
+    weight: '',
+    type: ''
+  });
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPokemon(event.target.value)
+  }
+
+  const fetchPokemon = async () => {
+    const normalizedPokemon = pokemon.toLowerCase();
+    const searchedPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${normalizedPokemon}`)
+    const pokeJson = await searchedPoke.json()
+    console.log(pokeJson)
+    const pokeType = pokeJson.types[0].type.name // ToDo: Handle pokemon with more than one type
+    const pokeObject = {
+      name: pokeJson.name,
+      height: pokeJson.height,
+      weight: pokeJson.weight,
+      type: pokeType
+    };
+    setPokeObject(pokeObject);
+  }
+
+  const searchForPokemon = () => {
+    if (pokemon === '') {
+      alert('Please enter a valid pokemon name and try again')
+    } else {
+      fetchPokemon();
+      setPokemon("");
+    }
+  }
+
+  return (
+    <ChakraProvider theme={theme}>
+      <div className='app-container'>
+        <PokeInput pokemon={pokemon} search={searchForPokemon} onChange={onChange} />
+        <PokeCard name={pokeObject.name} weight={pokeObject.weight} height={pokeObject.height} type={pokeObject.type} />
+      </div>
+    </ChakraProvider>
+  )
 }
-
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <div className='app-container'>
-      <PokeInput />
-      <PokeCard name={pokeObject.name} weight={pokeObject.weight} height={pokeObject.height} type={pokeObject.type} />
-    </div>
-  </ChakraProvider>
-)
--- src/components/PokeInput.tsx
+++ src/components/PokeInput.tsx
@@ -1,36 +1,20 @@
 import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
+interface PokeInputProps {
+    pokemon: string;
+    search: () => void;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-export const PokeInput = () => {
-    const [pokemon, setPokemon] = useState('');
-
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPokemon(event.target.value)
-    }
-
-    const fetchPokemon = async () => {
-        const normalizedPokemon = pokemon.toLowerCase();
-        const searchedPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${normalizedPokemon}`)
-        const pokeJson = await searchedPoke.json()
-        console.log(pokeJson)
-    }
-
-    const searchForPokemon = () => {
-        if (pokemon === '') {
-            alert('Please enter a valid pokemon name and try again')
-        } else {
-            fetchPokemon();
-            setPokemon("");
-        }
-    }
-
+export const PokeInput = (props: PokeInputProps) => {
+    const { pokemon, search, onChange } = props
     return (
         <div className="poke-search-bar">
             <InputGroup>
                 <Input placeholder="Search for a pokemon" value={pokemon} onChange={onChange} />;
                 <InputRightElement>
-                    <Button onClick={searchForPokemon}>
+                    <Button onClick={search}>
                         Go!
                     </Button>
                 </InputRightElement>
